b6960f8349f8bebdead2be46954c22447b5e5372
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2023-04-27 09:27:13 +0200

    lagt till genre fetch med axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,45 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [movie, setMovie] = useState([]);
-  const [movie2, setMovie2] = useState([]);
+  const [movieGenre, setMovieGenre] = useState([]);
   const [movie3, setMovie3] = useState([]);
   const [movieName, setMovieName] = useState("");
-
+  const [movie_id, setMovie_id] = useState(16);
   const handleMovieNameChange = (event) => {
     setMovieName(event.target.value);
   };
-  const FetchMovies = () => {
-    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=9bf8866aec070a01073c600a88bbefb5&query=${encodeURIComponent(
-      movieName
-    )}`;
-
-    fetch(apiUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setMovie3(data.results);
-        console.log("visar bild 3", movie3);
-      });
+  const genreClick = () => {
+    setMovie_id(28);
   };
   useEffect(() => {
-    FetchMovies();
-  }, [movieName]);
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=9bf8866aec070a01073c600a88bbefb5&with_genres=${encodeURIComponent(
+      movie_id
+    )} `;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setMovieGenre(response.data.results);
+        console.log("visar bild 3", movieGenre[1].backdrop_path);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [movie_id]);
 
   return (
     <div className="App">
-      <input
-        type="text"
-        placeholder="Skriv in en filmtitel"
-        value={movieName}
-        onChange={handleMovieNameChange}
-      />
-      {movie3.map((movies) => (
-        <div>
+      <button onClick={genreClick}> click</button>
+      <div>
+        {movieGenre.map((movies) => (
           <img
-            src={`https://image.tmdb.org/t/p/w500${movies.poster_path}`}
+            src={`https://image.tmdb.org/t/p/w500${movies.backdrop_path}`}
             alt="Movie poster"
           />
-        </div>
-      ))}
-      <div>
-        {movie2.map((movies2) => {
-          return (
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movies2.file_path}`}
-              alt="Movie poster"
-            />
-          );
-        })}
+        ))}
       </div>
     </div>
   );
 }
-
 export default App;
